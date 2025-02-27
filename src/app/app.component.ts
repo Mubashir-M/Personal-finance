e9a85a2d04c8b1850ed9eba85fd6dcb20d62e646
Mubashir-M <mubashir.mohamud@gmail.com>
2025-02-27 14:05:50 +0200

    add routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FinanceChartComponent } from './components/finance-chart/finance-chart.component';
-import { SavingsComponent } from './components/savings/savings.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    SidebarComponent,
-    FinanceChartComponent,
-    SavingsComponent,
-  ],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'finance-ai-app';
+
+  sidebarCollapsed = false;
+
+  onSidebarToggle(collapsed: boolean) {
+    this.sidebarCollapsed = !collapsed;
+  }
 }
